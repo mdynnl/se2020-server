fbbdb80e4d57f645af6584601d5a6d51391e26a3
Nyi Nyi Lwin <53917354+mdynnl@users.noreply.github.com>
2020-11-07 08:40:42 +0630

    update

---
+++ src/scripts/check.js
@@ -0,0 +1,17 @@
+require('dotenv-safe').config()
+const fetch = require('isomorphic-fetch')
+const { mongoose } = require('../app')
+let { WAREHOUSE_API, FILE_API } = process.env
+console.log('running pretest checks')
+
+console.log('checking servers: WarehouseAPI, MongoDB')
+fetch(WAREHOUSE_API)
+  .then(() => console.log('WarehouseAPI connected...'))
+  .catch(err => {
+    if (err.code === 'ECONNREFUSED') {
+      console.error('WarehouseAPI: failed to connect...')
+      process.exit(1)
+    }
+  })
+  
+mongoose.connect().finally(mongoose.disconnect)
